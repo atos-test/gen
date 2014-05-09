77b3299f3963c0e62544c6c3fd55b536dbd848d3
test-atos <a.gomezmart@hotmail.com>
2014-05-09 13:30:06 +0200

    referencias

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ C:\wamp\www\gen\platforms\android\res\drawable-mdpi\icon.png \
 C:\wamp\www\gen\platforms\android\res\drawable-xhdpi\icon.png \
 C:\wamp\www\gen\platforms\android\res\values\strings.xml \
 C:\wamp\www\gen\platforms\android\res\xml\config.xml \
+C:\wamp\www\gen\platforms\android\ant-build\res\drawable\icon.png \
+C:\wamp\www\gen\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+C:\wamp\www\gen\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+C:\wamp\www\gen\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+C:\wamp\www\gen\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\wamp\www\gen\platforms\android\ant-build\AndroidManifest.xml \
