72c943fe85db6b6294952d05428c0c285f1b9f95
test-atos <a.gomezmart@hotmail.com>
2014-05-12 10:22:36 +0200

    Carga XML

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,23 @@
 var app = {
-    // Application Constructor
+
     initialize: function() {
-        this.bindEvents();
+        var self = app;
+        app.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
+        
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        $("#btnIr").off("vclick").on("vclick", function(){
+            window.location.href = "lista.html";
+            //$.mobile.pageContainer.pagecontainer("change", "lista.html", { transition: "slide", reverse: false });
+        });
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
