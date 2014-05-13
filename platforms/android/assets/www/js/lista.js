69d9607b1d89dc160edbe27302bc310e4a07de61
test-atos <a.gomezmart@hotmail.com>
2014-05-13 11:01:01 +0200

    Portrait/Landscape

--- platforms/android/assets/www/js/lista.js
+++ platforms/android/assets/www/js/lista.js
@@ -3,9 +3,12 @@ var lista = {
     hojaContactos: null,
     contactos: null,
     anchoDocument: 0,
+    alturaDocument: 0,
 
     initialize: function() {
         var self = lista;
+        self.alturaDocument = $(document).height();
+        self.anchoDocument = $(document).width();
         self.loadXML();
         self.refreshView();
         self.bindEvents();
@@ -18,16 +21,54 @@ var lista = {
             self.hojaContactos = dataJSON;
             self.contactos = self.hojaContactos.chojaContactos.Contactos.cContacto;
         });
-
-        console.log("Lista: ", self.contactos);
     },
 
     refreshView: function(){
         var self = lista;
 
+        //Si estoy en LANDSCAPE cargo un template para detalle
+        if(window.orientation == -90 || window.orientation == 90) {
+
+            self.cargaLandscape();
+
+        }
+        //Si estoy en PORTRAIT cargo los templates hechos anteriormente
+        else{
+
+            self.cargaPortrait();
+        }
+
+    },
+
+    cargaLandscape: function(){
+        var self = lista;
+
+        self.cargaLista();
+    },
+
+    cargaPortrait: function(){
+        var self = lista;
+
+        self.cargaLista();
+    },
+
+    cargaLista: function(){
+        var self = lista; 
+
         self.cargaTemplateLista();
 
         self.resizeImg();
+    },
+
+    actualizarAltoAncho: function(){
+        var self = lista;
+
+        var aux = self.alturaDocument;
+        self.alturaDocument = self.anchoDocument;
+        self.anchoDocument = aux;
+
+        console.log("Ancho: ", self.anchoDocument);
+        console.log("Alto: ", self.alturaDocument);
     },
 
     resizeImg: function(){
@@ -36,14 +77,13 @@ var lista = {
         $("#listaContactos").listview().listview('refresh');
 
         /*El ancho de la lista es el 95% del ancho de la pantalla*/
-        self.anchoDocument = $(document).width();
         var anchoLista = self.anchoDocument * 0.95;
 
         /*El 5% restante se reparte entre padding-left y padding-right*/
         var anchoRestanteLista = self.anchoDocument - anchoLista;
-        $("#listaContactos").css({ 'padding-left': anchoRestanteLista/2});
-        $("#listaContactos").css({ 'padding-right': anchoRestanteLista/2});
-        $("#listaContactos").width(anchoLista);
+        $(".contentLista").css({ 'padding-left': anchoRestanteLista/2});
+        $(".contentLista").css({ 'padding-right': anchoRestanteLista/2});
+        $(".contentLista").width(anchoLista);
 
         /*El ancho de la imagen es el 25% de cada elemento*/
         var anchoImg = anchoLista*0.25;
@@ -51,6 +91,11 @@ var lista = {
 
         /*El alto de cada elemento de la lista es el alto de la imagen*/
         $(".elementoLista").height(anchoImg);
+
+        /*Calculamos el tamaño de la letra de la información del contacto*/
+        /*El 12% del ancho de la imagen*/
+        var tamLetra = anchoImg*0.012 + "em";
+        $(".letraContacto").css({ 'font-size': tamLetra});
 
         console.log("anchoDocument: ", self.anchoDocument);
         console.log("anchoLista: ", anchoLista);
@@ -75,6 +120,8 @@ var lista = {
     },
 
     bindEvents: function() {
+        var self = lista;
+
         $("#btnPanel").on("vclick", function(){
             $("#mypanel").panel( "open");
         });
@@ -87,5 +134,30 @@ var lista = {
                 console.log("Click: ", this.id);
             });
         });
+
+        /*Capturamos el evento de click de cada imagen*/
+        var contacto = document.getElementsByName("contactoName");
+
+        $(contacto).each(function(index){
+            $(this).on("vclick",function(){
+                console.log("Click: ", this.id);
+            });
+        });
+
+        window.onorientationchange = function() {
+            self.actualizarAltoAncho();
+            //Si estoy en LANDSCAPE cargo un template para detalle
+            if(window.orientation == -90 || window.orientation == 90) {
+
+                self.cargaLandscape();
+
+            }
+            //Si estoy en PORTRAIT cargo los templates hechos anteriormente
+            else{
+                
+                self.cargaPortrait();
+            }
+            self.bindEvents();
+        }
     },
 };
